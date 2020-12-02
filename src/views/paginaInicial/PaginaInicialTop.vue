0af837bafe264f1fcac4f248710bc3e70ec30087
Kevin <kevin.souza.94@gmail.com>
2020-12-02 02:23:21 -0300

    Centraliza logo na página inicial

--- src/views/paginaInicial/PaginaInicialTop.vue
+++ src/views/paginaInicial/PaginaInicialTop.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="section imagem-topo mt-5">
-    <div class="">
-      <div class="container">
         <div class="referencia">
-          <img src="@/assets/imagens/logo_total.png" alt="logo principal" /><br>
+          <img src="@/assets/imagens/logo_total.png" alt="logo principal">
           <!-- <div class="btn">
             <a href=""><b-button class="btn-home">Empresa</b-button></a>
             <a href=""><b-button class="btn-home">Usuário</b-button></a>
           </div> -->
         </div>
-      </div>
-    </div>
   </div>
 </template>
 
