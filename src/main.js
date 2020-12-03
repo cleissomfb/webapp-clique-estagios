77f4172aa0ae4b03acb48b31b41f7a1a3e728e24
cleissomfb <cleissom75@gmail.com>
2020-12-02 21:08:33 -0300

    Corrigido o layout responsivo da pagina de Contato.vue

--- src/main.js
+++ src/main.js
@@ -4,32 +4,43 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import 'babel-polyfill'
+// Imports do Fontawson - Não utilizados
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Import do CSS do estilo o painelAdm
 import './assets/css/estiloPainelAdm.css'
+
+// Import do CSS do estilo da PaginaPrincipal
 import './assets/css/estiloPaginaPrincipal.css'
+
+// Import das resoluções 
 import './assets/css/media500px.css'
 import './assets/css/media720px.css'
 import './assets/css/media1080px.css'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './assets/css/media1200px.css'
+
+// Import do bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import das maskaras de imput
 import VueMask from 'v-mask'
+
+// Import da validação dos campos
 import Vuelidate from 'vuelidate'
+
+// Import do slider para ver as vagas disponíveis index do site
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
-// import style (>= Swiper 6.x)
-// import 'swiper/swiper-bundle.css'
-
 Vue.use(VueAwesomeSwiper)
-
 Vue.use(Vuelidate)
 Vue.use(VueMask)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
